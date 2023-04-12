1b4ae39f3bc97bb4c1ed2ca97d0380e2dfea3f2b
Matthias Wild <matthias@wildpeter.de>
2023-04-12 18:38:55 +0200

    added logging

--- index.js
+++ index.js
@@ -5,14 +5,30 @@ const readfile = require("read-file");
 const fs = require('fs');
 const { base64encode, base64decode } = require('nodejs-base64');
 const { Synchronized }  = require("node-synchronized");
+const winston = require('winston');
+
+var logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json()
+    ),
+    transports: [
+      new winston.transports.Console({'timestamp': true}),
+    ],
+});
+
+exports.setLogger = function(_logger){
+    logger = _logger;
+}
 
 const block = new Synchronized();
 const pki = forge.pki;
 
-const keystore_filename = "keystore.secret";
-const publickey_filename = "publickey.secret";
-const privatekey_filename = "privatekey.secret";
-const keyEncryptionKey_filename = "keyEncryptionKey.secret";
+const keystore_filename = "keystore/keystore";
+const publickey_filename = "keystore/publickey";
+const privatekey_filename = "keystore/privatekey";
+const keyEncryptionKey_filename = "keystore/keyEncryptionKey.secret";
 
 exports.setKeystoreFilename = function(keystore_filename){
     global.keystore_filename = keystore_filename;
@@ -43,6 +59,7 @@ exports.getPassword = function(username){
 }
 
 function createKeypair(){
+    logger.verbose("createKeypair()");
     const keypair = pki.rsa.generateKeyPair();
     const keyEncryptionKey = forge.random.getBytesSync(32);
     
@@ -50,34 +67,44 @@ function createKeypair(){
     const encryptedPrivatePem = pki.encryptRsaPrivateKey(keypair.privateKey, keyEncryptionKey, {algorithm: 'aes256'});
     const keyEncryptionKeyBase64 = base64encode(keyEncryptionKey);
 
+    logger.info("Write new Keys to file");
     write.sync(publickey_filename, publicPem);
     write.sync(privatekey_filename, encryptedPrivatePem);
     write.sync(keyEncryptionKey_filename, keyEncryptionKeyBase64);
-    console.log("generated new keypairs and wrote them to the files. Please consider securing the keyEncryptionKey.secret at another place, so no intruder can decrypt the passwords");
+    logger.warn("generated new keypairs and wrote them to the files. Please consider securing the keyEncryptionKey.secret at another place, so no intruder can decrypt the passwords");
 }
 
 function insertPassword(username, password){
+    logger.verbose("insertPassword(${username}, ...)");
     block.Synchronized(first => {
+        logger.debug("read publickey from file");
         const publicKey = pki.publicKeyFromPem(readfile.sync(publickey_filename, 'utf8'));
 
         var keystore;
         if(fs.existsSync(keystore_filename)){
+            logger.debug("read keystore file");
             keystore = jsonfile.readFileSync(keystore_filename, 'utf8');
         } else {
+            logger.debug("no keystore file found");
             createKeypair();
             keystore = {};
         }
         
         keystore[username] = base64encode(publicKey.encrypt(password));
+        logger.debug("write keystore to file");
         jsonfile.writeFileSync(keystore_filename, keystore, { spaces: 2, EOL: '\r\n' });
     });
  }
 
 async function getPassword(username){
+    logger.verbose("getPassword(${username}, ...)");
+
     var keyEncryptionKeyBase64;
     if(fs.existsSync(keystore_filename)){
+        logger.debug("read key encryption key from file");
         keyEncryptionKeyBase64 = readfile.sync(keyEncryptionKey_filename, 'utf8');
     } else {
+        logger.debug("read key encryption key from user input");
         try {
             const result = await read({
                 "prompt": "no keyEncryptionKey File found, please enter the key in base64-format here:",
@@ -87,17 +114,20 @@ async function getPassword(username){
                 keyEncryptionKeyBase64 = input;
             })
         } catch (er) {
-            console.error(er);
-            console.error("cannot get Password");
+            logger.error(er);
+            logger.error("cannot get Password");
             return;
         }
     }
 
+    logger.debug("read encrypted private key from file");
     const encryptedPrivatePem = readfile.sync(privatekey_filename, 'utf8');
     const privateKey = pki.decryptRsaPrivateKey(encryptedPrivatePem, base64decode(keyEncryptionKeyBase64));
 
     block.Synchronized(first => {
+        logger.debug("read keystore from file");
         const keystore = jsonfile.readFileSync(keystore_filename, 'utf8');
     });
+    logger.debug("return password from keystore");
     return privateKey.decrypt(base64decode(keystore[username]));
 }
